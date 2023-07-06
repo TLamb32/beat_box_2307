d3db49b2d6f7323db6d3ff8e070a138b09dad1b3
Terry Lamb <terrylamb@gmail.com>
2023-07-06 18:16:15 -0400

    create node file and node test file

---
+++ spec/node_spec.rb
@@ -0,0 +1,12 @@
+require "./lib/node"
+
+RSpec.describe Node do
+  describe "iteration 1" do
+    it "exists and has readable attributes" do
+      node = Node.new("plop")
+
+      expect(node.data).to eq("plop")
+      expect(node.next_node).to eq(nil)
+    end
+  end
+end
